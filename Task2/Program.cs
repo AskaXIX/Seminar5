c091d19d57bf91e944b8cd493e576bc2f83d7e3b
Aslan <aslan.aslan.14@bk.ru>
2023-05-28 04:17:26 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,22 @@
 
 // [-4, -6, 89, 6] -> 0
 
+int[] arr = new int[10];
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(100, 1000);
+}
+
+int count = 0;
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] % 2 == 0)
+    {
+        count+=arr[i];
+    }
+}
+
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+System.Console.WriteLine($"Count: {count}");
